c567ff58875c84a772ae4c8f20b9121896bc6632
LucasBRKRL <LucasBRKRL@users.noreply.github.com>
2021-06-16 23:57:49 -0300

    inserindo dados no banco

--- src/main/java/com/viagem/api/config/TestConfig.java
+++ src/main/java/com/viagem/api/config/TestConfig.java
@@ -30,20 +30,25 @@ public class TestConfig implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Estado est1 = new Estado(null, "Goiás");
-		Estado est2 = new Estado(null, "São Paulo");
-		Estado est3 = new Estado(null, "Rio de Janeiro");
-		Estado est4 = new Estado(null, "Distrito Federal");
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Rio de Janeiro");
+		Estado est3 = new Estado(null, "Distrito Federal");
 		
-		Cidade cid1 = new Cidade(null, "Goiania", 1466105.00 , 10.00, est1);
-		Cidade cid2 = new Cidade(null, "Goiania", 1466105.00 , 40.00, est1);
-		Cidade cid3 = new Cidade(null, "Goiania", 1466105.00 , 40.00, est1);
+		Cidade cid1 = new Cidade(null, "Santos", 412621.99 , 11.00, est1);
+		Cidade cid2 = new Cidade(null, "Niterói", 159899.29 , 7.00, est2);
+		Cidade cid3 = new Cidade(null, "Brasília", 30551.49 , 23.00, est3);
 		
-		Hotel hot1 = new Hotel(null, "Golden", 150, 60.00, cid1);
 		
-		estado.saveAll(Arrays.asList(est1, est2, est3, est4));
+		Hotel hot1 = new Hotel(null, "Golden", 200, 60.00, cid3);
+		Hotel hot2 = new Hotel(null, "Excelsior", 175, 40.00, cid3);
+		Hotel hot3 = new Hotel(null, "Prime", 500, 20.00, cid1);
+		Hotel hot4 = new Hotel(null, "Cooper", 270, 70.00, cid1);
+		Hotel hot5 = new Hotel(null, "Plaza", 800, 30.00, cid2);
+		Hotel hot6 = new Hotel(null, "Terraço", 320, 60.00, cid2);
+		
+		estado.saveAll(Arrays.asList(est1, est2, est3));
 		cidade.saveAll(Arrays.asList(cid1, cid2, cid3));
-		hotel.saveAll(Arrays.asList(hot1));
+		hotel.saveAll(Arrays.asList(hot1, hot2, hot3, hot4, hot5, hot6));
 	} 
 
 }
